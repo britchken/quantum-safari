80b383739e78e3b6e52ff68e09ed5e004a737b6b
Brian Ritchken <britchken@gmail.com>
2023-04-12 23:40:08 -0700

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,17 +1,19 @@
 <?php
 $file = "counter.txt"; // path to the text file that will store the view count
 
-// open the file with exclusive lock
-$handle = fopen($file, "c+");
-if (flock($handle, LOCK_EX)) {
-    $count = intval(fread($handle, filesize($file))); // get the current count from the text file
-    $count++; // increment the count by 1
-    ftruncate($handle, 0); // truncate the file
-    fwrite($handle, $count); // write the new count back to the text file
-    flock($handle, LOCK_UN); // release the lock
+// check if a cookie has been set for this page
+if (isset($_COOKIE['page_viewed'])) {
+  $count = intval(file_get_contents($file)); // read the current count from the file
+} else {
+  // increment the count by 1
+  $count = intval(file_get_contents($file)) + 1;
+
+  // write the new count back to the file
+  file_put_contents($file, $count, LOCK_EX | LOCK_NB);
+
+  // set a cookie to prevent the count from being incremented on page refreshes
+  setcookie('page_viewed', true, time() + 3600 * 24); // expires after 24 hours
 }
-fclose($handle); // close the file
 
 echo "Views: " . $count; // display the count on the webpage
 ?>
-
