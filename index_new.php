938cd92b22985ebeae6bcbf1205f18505d2a72a4
Brian Ritchken <britchken@gmail.com>
2023-09-19 23:39:49 -0700

    initial commit
    asdf

---
+++ index_new.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML>
+<!--
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+	<meta  http-equiv="Content-Type" content="application;charset=UTF-8">
+		<meta name="description" content="The home of Quantum Safari, an up-and-coming musical genius.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="assets/css/home.css" />
+		<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />		
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Creepster">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mansalva">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Londrina Solid">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Boogaloo">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Coiny">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Eater">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<div id="header">
+
+				<div class="top">
+
+					<!-- Logo -->
+						<div id="logo">
+							<img src="./images/logo.png" style="width:100%"></img>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="#top" id="top-link"><span class="icon solid fa-home">Welcome</span></a></li>
+								<li><a href="#mole-park" id="mole-park-link"><span class="icon solid fa-hammer">Mole Park</span></a></li>
+								<li><a href="#lights-out" id="portfolio-link"><span class="icon solid fa-ghost">Lights Out</span></a></li>
+								<li><a href="#about" id="about-link"><span class="icon solid fa-music">Longo Bongos</span></a></li>
+								<li><a href="#contact" id="contact-link"><span class="icon solid fa-envelope">Contact</span></a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+				<div class="bottom">
+
+					<!-- Social Icons -->
+						<ul class="icons">
+							<a href="https://open.spotify.com/album/6W1sVGVI4aLiHebCFLGOSh?si=GCygTtpQTtu4VTmNWQCzDA" class="fa fa-spotify" style=" text-decoration:none;"></a>
+							<a href="https://www.instagram.com/quantumsafari/" class="fa fa-instagram" style=" text-decoration:none;"></a>
+						</ul>
+
+				</div>
+
+			</div>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- Intro -->
+					<section id="top" class="one dark cover">
+						<div class="container" height="100%">
+
+							
+							<div style="position:relative; top:5%; width:100%; text-align: center; font-size:3vmin;" >
+								<font class="ghost" style="font-size:3vmin">ooooohhhh hellooooooooo helloooo ohh ho hoooo hellooooooo<br>
+								yoooou are <?php include("counter.php"); ?>! that's a gooooooood one...<br><br>
+								</font>
+							</div>
+							<font class="boy" style="font-size:4vmin;">Quantum Safari here! Check out my newest release:<br></font><br><br>
+							<a href="halloween.php">
+								<img src="art_high_quality.png" style="width:40vmin; border-radius:20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"></img>
+								</a>
+								<h1><font class="neonText" style="font-size:4vh; font-family:Londrina Solid; text-decoration-line: underline;"><a href="halloween.php">Lights Out: A Halloween Adventure</a></font></h1><br><br>
+
+							<footer>
+								<a href="#portfolio" class="button scrolly">Magna Aliquam</a>
+							</footer>
+
+						</div>
+					</section>
+
+				<!-- Portfolio -->
+					<section id="lights-out" class="two">
+						<div class="container">
+
+
+						</div>
+					</section>
+
+				<!-- About Me -->
+					<section id="about" class="three">
+						<div class="container">
+
+							<header>
+								<h2>About Me</h2>
+							</header>
+
+							<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+
+							<p>Tincidunt eu elit diam magnis pretium accumsan etiam id urna. Ridiculus
+							ultricies curae quis et rhoncus velit. Lobortis elementum aliquet nec vitae
+							laoreet eget cubilia quam non etiam odio tincidunt montes. Elementum sem
+							parturient nulla quam placerat viverra mauris non cum elit tempus ullamcorper
+							dolor. Libero rutrum ut lacinia donec curae mus vel quisque sociis nec
+							ornare iaculis.</p>
+
+						</div>
+					</section>
+
+				<!-- Contact -->
+					<section id="contact" class="four">
+						<div class="container">
+
+							<header>
+								<h2>Contact</h2>
+							</header>
+
+							<p>Elementum sem parturient nulla quam placerat viverra
+							mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
+							donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
+							orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
+
+							<form method="post" action="#">
+								<div class="row">
+									<div class="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name" /></div>
+									<div class="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email" /></div>
+									<div class="col-12">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+									<div class="col-12">
+										<input type="submit" value="Send Message" />
+									</div>
+								</div>
+							</form>
+
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+		<!-- http://html5up.net -->
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
